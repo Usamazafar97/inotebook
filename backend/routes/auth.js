4e69e11cacfcdce5aec64650d29f473e253c5f8a
Usama Zafar <osamazafar97gmail.com>
2024-10-05 18:01:03 +0200

    validation added for email

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const {body, validationResult} = require('express-validator');
 
 
 // create a User using: POST "/api/auth/". Doesn't require Auth
-router.post('/', (req, res) => {
-    const user = User(req.body);
-    user.save()
-    res.send(req.body)
+router.post('/', 
+body('email').isEmail(),
+(req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors: errors.array()
+        });
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }).then(user => res.json(user))
+    .catch(error => {
+        console.log(error)
+        res.json({
+            error: 'Please enter a unique email'
+        })
+    })
 })
+
 
 
 module.exports = router
